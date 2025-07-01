732498544ca7a46cc140dbc090f605bc73bc7243
maurizio-dominici <mauriziodominici4@gmail.com>
2025-07-01 11:56:20 +0200

    edit HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
+import BestSellerSection from "../components/homepage_components/BestSellerSection";
+import HeroSection from "../components/homepage_components/HeroSection";
+import LatestArrivalSection from "../components/homepage_components/LatestArrivalsSection";
+
 export default function Homepage() {
   return (
     <div className="container">
       <h1>HomePage</h1>
+      <HeroSection />
+      <BestSellerSection />
+      <LatestArrivalSection />
     </div>
   );
 }
