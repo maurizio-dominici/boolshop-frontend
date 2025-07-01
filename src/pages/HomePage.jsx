b548a71da364d563cb8b83acbcd7fcfb25330583
EneaSema <sema.enea1993@gmail.com>
2025-07-01 16:25:33 +0200

    finish-best-seller-card-homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,101 @@ export default function Homepage() {
       {/* Creazione Sezione Componenti della Homepage */}
       <HeroSection />
       <BestSellerSection />
+      <div className="row">
+        <div className="col-3">
+          <div className="card h-100">
+            <img className="img-parfume" src="./vite.svg" alt="" />
+            <div className="card-title">
+              <h2>"ciao"</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+                quae autem ipsam laudantium, aliquid pariatur nam, magni itaque
+                exercitationem atque quos velit. Rem, alias illo vitae iure
+                quasi blanditiis necessitatibus. Neque sapiente impedit ea at
+                illo ipsum non architecto laborum? Non repellendus odio eaque,
+                laborum libero perferendis dicta consequuntur sit a, magni
+                ducimus, qui veniam quis doloribus est voluptate laboriosam?
+                Laudantium iure dicta aut sed architecto voluptatibus inventore
+                nobis. Quam quia eum officiis eligendi vero. Deleniti, incidunt
+                possimus tempore laboriosam vero, accusamus recusandae animi
+                nisi soluta earum optio voluptatibus qui!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card h-100">
+            <img className="img-parfume" src="./vite.svg" alt="" />
+            <div className="card-title">
+              <h2>"ciao"</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+                quae autem ipsam laudantium, aliquid pariatur nam, magni itaque
+                exercitationem atque quos velit. Rem, alias illo vitae iure
+                quasi blanditiis necessitatibus. Neque sapiente impedit ea at
+                illo ipsum non architecto laborum? Non repellendus odio eaque,
+                laborum libero perferendis dicta consequuntur sit a, magni
+                ducimus, qui veniam quis doloribus est voluptate laboriosam?
+                Laudantium iure dicta aut sed architecto voluptatibus inventore
+                nobis. Quam quia eum officiis eligendi vero. Deleniti, incidunt
+                possimus tempore laboriosam vero, accusamus recusandae animi
+                nisi soluta earum optio voluptatibus qui!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card h-100">
+            <img className="img-parfume" src="./vite.svg" alt="" />
+            <div className="card-title">
+              <h2>"ciao"</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+                quae autem ipsam laudantium, aliquid pariatur nam, magni itaque
+                exercitationem atque quos velit. Rem, alias illo vitae iure
+                quasi blanditiis necessitatibus. Neque sapiente impedit ea at
+                illo ipsum non architecto laborum? Non repellendus odio eaque,
+                laborum libero perferendis dicta consequuntur sit a, magni
+                ducimus, qui veniam quis doloribus est voluptate laboriosam?
+                Laudantium iure dicta aut sed architecto voluptatibus inventore
+                nobis. Quam quia eum officiis eligendi vero. Deleniti, incidunt
+                possimus tempore laboriosam vero, accusamus recusandae animi
+                nisi soluta earum optio voluptatibus qui!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card h-100">
+            <img className="img-parfume" src="./vite.svg" alt="" />
+            <div className="card-title">
+              <h2>"ciao"</h2>
+            </div>
+            <div className="card-body">
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga
+                quae autem ipsam laudantium, aliquid pariatur nam, magni itaque
+                exercitationem atque quos velit. Rem, alias illo vitae iure
+                quasi blanditiis necessitatibus. Neque sapiente impedit ea at
+                illo ipsum non architecto laborum? Non repellendus odio eaque,
+                laborum libero perferendis dicta consequuntur sit a, magni
+                ducimus, qui veniam quis doloribus est voluptate laboriosam?
+                Laudantium iure dicta aut sed architecto voluptatibus inventore
+                nobis. Quam quia eum officiis eligendi vero. Deleniti, incidunt
+                possimus tempore laboriosam vero, accusamus recusandae animi
+                nisi soluta earum optio voluptatibus qui!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <LatestArrivalSection />
     </div>
   );
