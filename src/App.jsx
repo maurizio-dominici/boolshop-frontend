6b2e1400ba0d263776004a367a0beefaa30a7728
yuricifiello <yuricifiello@gmail.com>
2025-07-03 17:46:16 +0200

    add-detailspage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import AboutUs from "./pages/AboutUs";
-import Homepage from "./pages/HomePage";
+import HomePage from "./pages/HomePage";
 import BestSellersSectionListPage from "./pages/section_pages/best_seller_pages/BestSellersSectionListPage";
 import LatestArrivalsSectionListPage from "./pages/section_pages/latest_sellers_pages/LatestArrivalsSectionListPage";
 import { ParfumeAPIProvider } from "./context/ParfumesContext";
 import SearchResults from "./pages/section_pages/hero_section_pages/SearchResults";
-import ProductDetailsPage from "./pages/section_pages/latest_sellers_pages/ProductDetailsPageRecents";
+import ProductDetailsPage from "./components/homepage_components/ProductDetailsPage";
 
 export default function App() {
   return (
@@ -14,23 +14,21 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
-            <Route index element={<Homepage />} />
-            <Route path="/parfumes" element={<SearchResults />} />
+            <Route index element={<HomePage />} />
             <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/search" element={<SearchResults />} />
 
-            {/* Lista dei Best Seller */}
+            {/* Sezioni prodotto */}
             <Route
               path="/bestsellers"
               element={<BestSellersSectionListPage />}
             />
-
-            {/* Lista degli Ultimi Arrivi */}
             <Route
               path="/recents"
               element={<LatestArrivalsSectionListPage />}
             />
 
-            {/* Dettagli del prodotto (valido sia per bestsellers che recents) */}
+            {/* Dettaglio prodotto */}
             <Route path="/product/:id" element={<ProductDetailsPage />} />
           </Route>
         </Routes>
