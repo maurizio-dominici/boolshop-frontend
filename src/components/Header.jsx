7e1cc91a68495e910bf5a35d57e0bdc82a1e83c4
EneaSema <sema.enea1993@gmail.com>
2025-07-07 17:51:44 +0200

    add-link-cart-icon-navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../components/Navbar";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <>
       <div className="d-flex justify-content-between align-items-center bg-body-tertiary">
         <Navbar />
-        <i class="bi bi-cart3 m-3 icon-xl"></i>
+        <NavLink
+          to={"/cart"}
+          className="nav-link bi bi-cart3 m-3 icon-xl"
+        ></NavLink>
       </div>
     </>
   );
