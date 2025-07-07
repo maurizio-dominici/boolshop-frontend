dbfa730fc121c54602baa67be9f44a1aaa23eb55
EneaSema <sema.enea1993@gmail.com>
2025-07-07 16:33:50 +0200

    add-cart-icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import Navbar from "../components/Navbar";
 export default function Header() {
   return (
     <>
-      <Navbar />
+      <div className="d-flex justify-content-between align-items-center bg-body-tertiary">
+        <Navbar />
+        <i class="bi bi-cart3 m-3 icon-xl"></i>
+      </div>
     </>
   );
 }
