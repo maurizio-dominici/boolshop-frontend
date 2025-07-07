4bbda22522e4bc500e84b932e10278b8fc4ec6ea
fedebacci <federicobacci1999@gmail.com>
2025-07-07 17:49:41 +0200

    CartPopup created

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import BestSellersSectionListPage from "./pages/section_pages/best_seller_pages/
 import LatestArrivalsSectionListPage from "./pages/section_pages/latest_sellers_pages/LatestArrivalsSectionListPage";
 import { ParfumeAPIProvider } from "./context/ParfumesContext";
 import { TopMessageProvider } from "./context/TopMessageContext";
+import { CartPopupProvider } from "./context/CartPopupContext";
 import SearchResults from "./pages/section_pages/hero_section_pages/SearchResults";
 import ProductDetailsPage from "./components/homepage_components/ProductDetailsPage";
 import CheckoutPage from "./pages/section_pages/CheckoutPage";
@@ -18,37 +19,39 @@ import { stripePromise } from "./assets/stripe";
 export default function App() {
   return (
     <TopMessageProvider>
-      <ParfumeAPIProvider>
-        <BrowserRouter>
-          <Elements stripe={stripePromise}>
-            <Routes>
-              <Route element={<DefaultLayout />}>
-                <Route index element={<HomePage />} />
-                <Route path="/about-us" element={<AboutUs />} />
-                <Route path="/parfumes" element={<SearchResults />} />
-                <Route path="/checkout" element={<CheckoutPage />} />
-                <Route path="/recipt" element={<ReciptPage />} />
+      <CartPopupProvider>
+        <ParfumeAPIProvider>
+          <BrowserRouter>
+            <Elements stripe={stripePromise}>
+              <Routes>
+                <Route element={<DefaultLayout />}>
+                  <Route index element={<HomePage />} />
+                  <Route path="/about-us" element={<AboutUs />} />
+                  <Route path="/parfumes" element={<SearchResults />} />
+                  <Route path="/checkout" element={<CheckoutPage />} />
+                  <Route path="/recipt" element={<ReciptPage />} />
 
-                {/* Sezioni prodotto */}
-                <Route
-                  path="/bestsellers"
-                  element={<BestSellersSectionListPage />}
-                />
-                <Route
-                  path="/recents"
-                  element={<LatestArrivalsSectionListPage />}
-                />
-                <Route path="/cart" element={<CartPage />} />
+                  {/* Sezioni prodotto */}
+                  <Route
+                    path="/bestsellers"
+                    element={<BestSellersSectionListPage />}
+                  />
+                  <Route
+                    path="/recents"
+                    element={<LatestArrivalsSectionListPage />}
+                  />
+                  <Route path="/cart" element={<CartPage />} />
 
-                {/* Dettaglio prodotto */}
-                <Route path="/product/:slug" element={<ProductDetailsPage />} />
+                  {/* Dettaglio prodotto */}
+                  <Route path="/product/:slug" element={<ProductDetailsPage />} />
 
-                <Route path='*' element={<NotFoundPage />} />
-              </Route>
-            </Routes>
-          </Elements>
-        </BrowserRouter>
-      </ParfumeAPIProvider>
+                  <Route path='*' element={<NotFoundPage />} />
+                </Route>
+              </Routes>
+            </Elements>
+          </BrowserRouter>
+        </ParfumeAPIProvider>
+      </CartPopupProvider>
     </TopMessageProvider>
   );
 }
--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,12 +1,14 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import TopMessage from "../components/ui/TopMessage";
+import CartPopup from "../components/ui/CartPopup";
 import Footer from "../components/footer";
 
 export default function DefaultLayout() {
   return (
     <>
       <TopMessage />
+      <CartPopup />
       <Header />
       <Outlet />
       <Footer />
