23bded8ff59388f721da13b1e82715d1dd8b1d2e
fedebacci <federicobacci1999@gmail.com>
2025-07-08 11:34:10 +0200

    correction

--- src/components/homepage_components/Card.jsx
+++ src/components/homepage_components/Card.jsx
@@ -73,7 +73,7 @@ export default function Card({ item }) {
                 </>
               ) : (
                 <>
-                  B {item.price}€
+                  {item.price}€
                 </>
               )}
           </p>
