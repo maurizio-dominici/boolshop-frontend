0208c973b22cbc5ba411f7298a53b05719369ce3
fedebacci <federicobacci1999@gmail.com>
2025-07-10 22:08:20 +0200

    List visualization fixes

--- src/components/ui/ProductListVisualization.jsx
+++ src/components/ui/ProductListVisualization.jsx
@@ -15,19 +15,34 @@ export default function ProductListVisualization ({ products, title, text, link,
             </div> */}
 
 
-            <section className="py-5 bg-light">
+            {/* <section className="py-5 bg-light"> */}
+            {/* <section className="mt-4"> */}
+
+            {
+                !isHomepage ?
+                <div className="container mt-4"></div>
+                :
+                <div className="container mt-3"></div>
+            }
+            <section>
                 <div className="container">
-                    <div className="d-flex justify-content-between align-items-center mb-4">
+                    <div className="d-flex justify-content-between align-items-center mb-3">
                         {
-                            title &&
+                            (title && isHomepage) &&
                             <h2 className="fw-bold">
                                 {title}
                             </h2>
                         }
                         {
-                            link &&
+                            (link && isHomepage) &&
                             <Link to={link || "/parfumes"}>
                                 <button className="btn btn-primary">{text || "Visualizza tutti i profumi"}</button>
+                            </Link>
+                        }
+                        {
+                            !isHomepage &&
+                            <Link to={-1} className="btn btn-secondary">
+                                Torna indietro
                             </Link>
                         }
                     </div>
@@ -52,17 +67,6 @@ export default function ProductListVisualization ({ products, title, text, link,
                                             </div>
                                         ))
                                     }
-
-                                    {/* <div className="mt-4">
-                                        <Link to={'/'} className="btn btn-secondary">
-                                            Torna indietro
-                                        </Link>
-                                    </div> */}
-                                    <div className="mt-4">
-                                        <Link to={-1} className="btn btn-secondary">
-                                            Torna indietro
-                                        </Link>
-                                    </div>
                                 </>
                         }
                     </div>
